d45327007052926d7a0da20dddaaa2e89461417c
unknown <diegofbisquerra@gmail.com>
2022-10-14 13:13:35 -0300

    CP Model Done

--- models/model.js
+++ models/model.js
@@ -28,21 +28,46 @@ module.exports = {
     // El usuario debe guardarse como un objeto con el siguiente formato:
     // {  email: email, name: name,  plan: 'regular' , watched: []}
     // En caso exitoso debe retornar el string 'Usuario <email_del_usuario> creado correctamente'.
+    const userFound = users.find((u) => u.email === email)
 
+    if (userFound) {
+      throw new Error("El usuario ya existe")
+    }
+    else {
+      const user = { email: email, name: name, plan: "regular", watched: [] }
+      users.push(user)
+      return `Usuario ${email} creado correctamente`
+    }
   },
 
   listUsers: function (plan) {
     // Si no recibe parámetro, devuelve un arreglo con todos los usuarios.
     // En caso de recibir el parámetro <plan>, devuelve sólo los usuarios correspondientes a dicho plan ('regular' o 'premium').
-
+    if (plan) {
+      const usuariosPlan = users.filter(u =>
+        u.plan === plan)
+      return usuariosPlan
+    }
+    else {
+      return users
+    }
   },
 
   switchPlan: function (email) {
-  // Alterna el plan del usuario: si es 'regular' lo convierte a 'premium' y viceversa.
-  // Retorna el mensaje '<Nombre_de_usuario>, ahora tienes el plan <nuevo_plan>'
-  // Ej: 'Martu, ahora tienes el plan premium'
-  // Si el usuario no existe, arroja el Error ('Usuario inexistente')
+    // Alterna el plan del usuario: si es 'regular' lo convierte a 'premium' y viceversa.
+    // Retorna el mensaje '<Nombre_de_usuario>, ahora tienes el plan <nuevo_plan>'
+    // Ej: 'Martu, ahora tienes el plan premium'
+    // Si el usuario no existe, arroja el Error ('Usuario inexistente')
+    const userFound = users.find((u) => u.email === email)
 
+    if (userFound === undefined) {
+      throw new Error("Usuario inexistente")
+    }
+    else {
+      if (userFound.plan === "regular") userFound.plan = "premium"
+      else { userFound.plan = "regular" }
+      return `${userFound.name}, ahora tienes el plan ${userFound.plan}`
+    }
   },
 
   addSerie: function (name, seasons, category, year) {
@@ -53,14 +78,25 @@ module.exports = {
     // Debe guardar la propiedad <category> de la serie (regular o premium)
     // Debe guardar la propiedade <rating> inicializada 0
     // Debe guardar la propiedade <reviews> que incialmente es un array vacío.
+    if (series.find(e => e.name === name)) throw new Error(`La serie ${name} ya existe`);
 
+    if (categories[0] === category || categories[1] === category)
+      series.push({ name: name, seasons: seasons, category: category, rating: 0, year: year, reviews: [] });
+    
+    else throw new Error(`La categoría ${category} no existe`)
   },
 
   listSeries: function (category) {
     // Devuelve un arreglo con todas las series.
     // Si recibe una categoría como parámetro, debe filtrar sólo las series pertenecientes a la misma (regular o premium).
     // Si la categoría no existe, arroja un Error ('La categoría <nombre_de_la_categoría> no existe') y no agrega la serie.
+    if (category === undefined) return series;
 
+    if (categories[0] === category || categories[1] === category)
+      return series.filter(e => e.category === category)
+    
+    console.log(category);
+    throw new Error(`La categoría ${category} no existe`)
   },
 
   play: function (serie, email) {
@@ -71,13 +107,33 @@ module.exports = {
     // En caso de contrario arrojar el Error ('Contenido no disponible, contrata ahora HenryFlix Premium!')
     // En caso exitoso, añadir el nombre (solo el nombre) de la serie a la propiedad <watched> del usuario.
     // Devuelve un mensaje con el formato: 'Reproduciendo <nombre de serie>'
+    const userFound = users.find((u) => u.email === email)
+    const serieFound = series.find((s) => s.name === serie)
 
+    if (userFound === undefined) {
+      throw new Error("Usuario inexistente")
+    }
+    if (serieFound === undefined) {
+      throw new Error("Serie inexistente")
+    }
+
+    else if (userFound.plan === "regular" && serieFound.category === "premium") {
+      throw new Error("Contenido no disponible, contrata ahora HenryFlix Premium!")
+    }
+
+    else userFound.watched.push(serieFound.name)
+    return `Reproduciendo ${serie}`
   },
 
   watchAgain: function (email) {
     // Devuelve sólo las series ya vistas por el usuario
     // Si el usuario no existe, arroja el Error ('Usuario inexistente')
+    const userFound = users.find((u) => u.email === email)
 
+    if (userFound === undefined) {
+      throw new Error("Usuario inexistente")
+    }
+    return userFound.watched
   },
 
   rateSerie: function (serie, email, score) {
@@ -89,7 +145,30 @@ module.exports = {
     // Si la serie no existe, arroja el Error ('Serie inexistente') y no actualiza el puntaje.
     // Debe recibir un puntaje entre 1 y 5 inclusive. En caso contrario arroja el Error ('Puntaje inválido') y no actualiza el puntaje.
     // Si el usuario no reprodujo la serie, arroja el Error ('Debes reproducir el contenido para poder puntuarlo') y no actualiza el puntaje. >> Hint: pueden usar la función anterior
+    const userFound = users.find((u) => u.email === email)
+    const serieFound = series.find((s) => s.name === serie)
 
+    if (userFound === undefined) {
+      throw new Error("Usuario inexistente")
+    }
+    if (serieFound === undefined) {
+      throw new Error("Serie inexistente")
+    }
+    if (score > 5 || score < 1) throw new Error('Puntaje inválido');
+
+    if (!userFound.watched.find(e => e === serie)) {
+      throw new Error('Debes reproducir el contenido para poder puntuarlo')
+    } else {
+      serieFound.reviews.push({ email: email, score: score })
+
+      let contador = 0;
+      for (let i = 0; i < serieFound.reviews.length; i++) {
+        contador += serieFound.reviews[i].score
+      }
+
+      serieFound.rating = contador / serieFound.reviews.length
+      
+      return `Le has dado ${score} puntos a la serie ${serieFound.name}`
+    }
   }
-
 }
